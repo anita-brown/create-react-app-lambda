f98ed0169cca64d6a695fb5925bcf764b06c7232
anita-brown <anita.osagie@decagon.dev>
2022-02-03 08:21:34 +0100

    handleDelete in feedbacklist

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,16 +1,16 @@
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
-function FeedbackList({feedback}) {
+
+function FeedbackList({feedback, handleDelete}) {
     if (!feedback || feedback.length === 0){
         return <p>No Feedback Yet</p>
     }
 
   return (
-  <div className='feedback-list'>
-  {
-      feedback.map((item)=>(
-          <FeedbackItem key ={item.id} item={item}/>
-        
+  <div className='feedback-list'> 
+  {feedback.map((item)=>(
+          <FeedbackItem key={item.id} item={item}
+              handleDelete={handleDelete}/>
       ))}
   </div>
   ) 

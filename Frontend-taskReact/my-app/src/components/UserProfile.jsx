8794e84820f0311f0c9ad3821c1ff6729bf23a14
anita-brown <anita.osagie@decagon.dev>
2022-02-07 17:01:23 +0100

    userprofile changes

--- Frontend-taskReact/my-app/src/components/UserProfile.jsx
+++ Frontend-taskReact/my-app/src/components/UserProfile.jsx
@@ -3,6 +3,7 @@ import man2 from '../asset/img/man2.jpeg'
 import { useEffect, useState } from 'react'
 import { Link } from "react-router-dom";
 import { useCallback } from 'react'
+import Loaders from 'react-loading-icons'
 
 
 
@@ -20,6 +21,8 @@ import { useCallback } from 'react'
 
 const UserProfile = () => {
      const [angularOrg, setangularOrg] = useState([])
+     const [loading, setLoading] = useState(true);
+
     const fetchContributors = useCallback(async () => {
         const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/orgs/Angular`, {
             headers: {
@@ -27,10 +30,77 @@ const UserProfile = () => {
             }
         })
         const data = await response.json()
-        console.log(data)
          setangularOrg([data])
-      console.log(angularOrg)
 
+          const nextResponse = await fetch(`${process.env.REACT_APP_GITHUB_URL}/orgs/angular/repos?per_page=10`, {
+            headers: {
+                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+        })
+         const newData = await nextResponse.json()
+
+            const nextNextResponse = await fetch(`${process.env.REACT_APP_GITHUB_URL}/repos/angular/angular.js/contributors`, {
+                headers: {
+                    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+                }
+            })
+
+            const resultData = await nextNextResponse.json();
+        
+        console.log('ccc', resultData)
+
+        const repositories = resultData.map(async(element)=>{
+            const repoResponse = await fetch(element.repos_url, {
+                headers: {
+                     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+                }
+            })
+            return await repoResponse.json();
+        })
+        console.log('bb',repositories)
+
+const allRepoContributors = await Promise.all(repositories)
+console.log('iii', allRepoContributors)
+
+
+
+ const ranks = {};
+
+        allRepoContributors.forEach(repo => {
+            repo.forEach(repositories => {
+                if (!ranks[repositories.name]) {
+                    ranks[repositories.name] = {
+                        name: repositories.name,
+                        avatarUrl: repositories.owner.avatar_url,
+                        userUrl: repositories.url,
+                        contributions: repositories.contributors_url,
+                        followers: repositories.owner.followers_url,
+                        gists: repositories.owner.gists_url
+
+                    }
+                } else {
+                    ranks[repositories.name].contribution = ranks[repositories.name].contribution + repositories.contributions
+                }
+            })
+        })
+        console.log(Object.values(ranks)[0].userUrl)
+        const userPromises = Object.values(ranks).map(async (user) => {
+            const response = await fetch(user.userUrl, {
+                headers: {
+                    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+                }
+            })
+            return await response.json()
+        });
+        const allUser = await Promise.all(userPromises)
+         allUser.forEach(user => {
+            ranks[user.login] = { ...ranks[user.login], name: user.name, followers: user.followers, public_repos: user.public_repos, public_gists: user.public_gists }
+        })
+
+
+
+      
+         setLoading(false)
          }, [angularOrg]
     )
 
